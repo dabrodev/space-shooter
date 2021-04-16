faf1679277c09617bf0fd3aab6b8550b027871a2
Damian Dąbrowski <dabdamian@gmail.com>
2021-04-16 21:46:12 +0200

    Added basic movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private float _speed = 3.5f;
-    public float horizontalInput;
-    public float verticalInput;
+ 
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +16,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-      
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // new Vector(1, 0, 0) * -1 * 3.5f * real time 
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
 
     }
 }
