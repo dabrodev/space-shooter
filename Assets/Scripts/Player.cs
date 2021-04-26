5ead9334d754033efa9fa92c0edfd05166114889
Damian Dabrowski <dabdamian@gmail.com>
2021-04-26 21:51:27 +0200

    Added 3 functions for player  movemenet.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,11 +15,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       TeleportMovement();
-
+        PlayerMovement();
+        PlayerTeleport();
     }
 
-    void TeleportMovement() {
+    void PlayerMovement() {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
@@ -30,7 +30,9 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
-
+    }
+    void PlayerTeleport() 
+    {
         // teleport
         // if player horizontal position x is 9 or greater
         // the position.x = 9.26f
@@ -53,7 +55,6 @@ public class Player : MonoBehaviour
         
     }
     void RestrictMovement() {   
-        // restrict player movement
-        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9.26f,9.26f), Mathf.Clamp(transform.position.y, -4.0f,6.0f), 0);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9.26f,9.26f), Mathf.Clamp(transform.position.y, -4.0f,6.0f), 0);
     }
 }
