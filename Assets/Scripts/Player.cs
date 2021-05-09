be0ef8947178eac7d43e6d3607267302d355b22c
Damian Dabrowski <dabdamian@gmail.com>
2021-05-09 23:07:20 +0200

    Added OnPlaterDeath function.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,11 +15,18 @@ public class Player : MonoBehaviour
     private float _nextFire = -1.0f;
     [SerializeField]
     private int _lives = 3;
+    private SpawnManager _spawnManager;
 
 
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if (_spawnManager == null)
+        {
+            Debug.Log("The Spawn Manager is NULL");
+        }
     }
 
     void Update()
@@ -76,7 +83,9 @@ public class Player : MonoBehaviour
 
         if (_lives < 1)
         {
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
+            
         }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,7 @@ public class SpawnManager : MonoBehaviour
     private GameObject _enemyPrefab;
     [SerializeField]
     private GameObject _enemyContainer;
+    private bool _stopSpawning = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,14 +28,18 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnRoutine()
     {
-        while (true)
+        while (_stopSpawning == false)
         {
             float randomX = Random.Range(-9.5f, 9.5f);
             Vector3 posToSpawn = new Vector3(randomX, 7, 0);
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
-           
         }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
     }
 }
