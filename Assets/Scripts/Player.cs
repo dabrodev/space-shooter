cf037b3e8c127b059d538156c366b39d7623b6bf
Damian Dabrowski <dabdamian@gmail.com>
2021-05-16 23:11:57 +0200

    Laser from 3D to 2D.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameObject _laserPrefab;
     [SerializeField]
-    private Vector3 _laserOffset = new Vector3(0,0.8f,0);
+    private Vector3 _laserOffset = new Vector3(0,1.05f,0);
     [SerializeField]
     private float _fireRate = 0.15f;
     private float _nextFire = -1.0f;
