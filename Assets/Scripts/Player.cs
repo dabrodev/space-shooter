c1e5d0c0af3518851f7a8263c40dbeb48ddb7fcf
Damian Dabrowski <dabdamian@gmail.com>
2021-05-19 21:32:19 +0200

    Added Triple Shot.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameObject _laserPrefab;
     [SerializeField]
+    private GameObject _tripleShotPrefab;
+    [SerializeField]
     private Vector3 _laserOffset = new Vector3(0,1.05f,0);
     [SerializeField]
     private float _fireRate = 0.15f;
@@ -16,6 +18,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     private int _lives = 3;
     private SpawnManager _spawnManager;
+    [SerializeField]
+    private bool _isTripleShotActive = false;
 
 
     void Start()
@@ -72,8 +76,16 @@ public class Player : MonoBehaviour
         {
             
             _nextFire = Time.time + _fireRate;
+
+            if (_isTripleShotActive == true)
+            {
+                Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+            } else
+            {
+                Instantiate(_laserPrefab, transform.position + _laserOffset, Quaternion.identity);
+            }
             
-            Instantiate(_laserPrefab, transform.position + _laserOffset, Quaternion.identity);
+            
         }
     }
 
