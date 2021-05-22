975554cb53f910201891c32a9cc6d751900a9b68
Damian Dabrowski <dabdamian@gmail.com>
2021-05-22 22:41:53 +0200

    Added pseudocode to powerup script.

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -13,6 +13,11 @@ public class Powerup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //move down at a speed of 3 (adjust in the inspector)
+        //when we leave the screen, destroy the object
     }
+
+    //OnTriggerCollision
+    //Only be collectable by the Player
+    //on collected, destroy
 }
