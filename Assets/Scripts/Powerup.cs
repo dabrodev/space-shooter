eeedf77d763b7bf448e5b27bfb5cc5a3cd7d7744
Damian Dabrowski <dabdamian@gmail.com>
2021-05-23 15:44:00 +0200

    Added movement and trigger to powerup script.

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Powerup : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +16,22 @@ public class Powerup : MonoBehaviour
     void Update()
     {
         //move down at a speed of 3 (adjust in the inspector)
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
         //when we leave the screen, destroy the object
+        if (transform.position.y < -6.0) {
+            Destroy(this.gameObject);
+        }
     }
 
     //OnTriggerCollision
     //Only be collectable by the Player
     //on collected, destroy
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player");
+        {
+            
+            Destroy(this.gameObject);
+        }
+    }
 }
