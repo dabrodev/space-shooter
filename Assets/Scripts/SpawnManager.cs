e7640c5f7536f48fe31ba4e30ac89acccdfffa9f
Damian Dabrowski <dabdamian@gmail.com>
2021-05-23 18:50:14 +0200

    Added powerup spawner with coroutine.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,15 +7,20 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyPrefab;
     [SerializeField]
+    private GameObject _powerupPrefab;
+    [SerializeField]
     private GameObject _enemyContainer;
+    [SerializeField]
+    private GameObject _powerupContainer;
     private bool _stopSpawning = false;
 
     void Start()
     {
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
     }
 
-    IEnumerator SpawnRoutine()
+    IEnumerator SpawnEnemyRoutine()
     {
         while (_stopSpawning == false)
         {
@@ -24,6 +29,18 @@ public class SpawnManager : MonoBehaviour
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator SpawnPowerupRoutine() {
+        while (_stopSpawning == false)
+        {
+            float randomFreq = Random.Range(3.0f, 7.0f);
+            float randomX = Random.Range(-9.5f, 9.5f);
+            Vector3 posToSpawn = new Vector3(randomX, 7, 0);
+            GameObject newPowerup = Instantiate(_powerupPrefab, posToSpawn, Quaternion.identity);
+            newPowerup.transform.parent = _powerupContainer.transform;
+            yield return new WaitForSeconds(randomFreq);
         }
     }
 
