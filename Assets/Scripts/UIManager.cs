8903196a7bcd10af44b870cd7a4ec93e4dfa5d44
Damian Dabrowski <dabdamian@gmail.com>
2021-06-17 14:00:00 +0100

    Code cleanup.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,7 +11,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _scoreText.text = "Score: " + 0;
+        _scoreText.text = "Score: " + 10;
     }
 
     // Update is called once per frame
