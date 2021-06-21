682488221ca28f9fe8d4ad780e7aa337ba1e6555
Damian Dabrowski <dabdamian@gmail.com>
2021-06-21 14:00:00 +0100

    Code cleanup.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,11 +8,11 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Text _scoreText;
 
-    // Start is called before the first frame update
+   
     void Start()
     {
         _scoreText.text = "Score: " + 0;
-        // how to access Player Variable?
+        
     }
 
 
