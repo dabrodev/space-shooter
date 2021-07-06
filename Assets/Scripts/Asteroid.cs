30366dbb17a7c03952d9f5ba5701782b2f5ddef5
Damian Dabrowski <dabdamian@gmail.com>
2021-07-06 23:41:09 +0200

    Asteroid explosion.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -7,6 +7,9 @@ public class Asteroid : MonoBehaviour
 
     private float _speed = 3.0f;
     private float _smooth = 20.0f;
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,15 @@ public class Asteroid : MonoBehaviour
         //transform.Rotate(0, 0, 10 * _smooth * Time.deltaTime, Space.World);
 
         transform.Rotate(Vector3.forward * _speed * Time.deltaTime);
+        
        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+        }
     }
 }
