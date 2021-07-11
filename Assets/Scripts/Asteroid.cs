aed326c45d181e37ea7ec88a107ff51cdd72ec55
Damian Dabrowski <dabdamian@gmail.com>
2021-07-11 22:46:06 +0200

    Code cleanup.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,28 +4,23 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-
     private float _speed = 8.0f;
     //private float _smooth = 20.0f;
     [SerializeField]
     private GameObject _explosionPrefab;
     private SpawnManager _spawnManager;
 
-    // Start is called before the first frame update
     void Start()
     {
         _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         //transform.Translate(Vector3.down * _speed * Time.deltaTime, Space.World);
         //transform.Rotate(0, 0, 10 * _smooth * Time.deltaTime, Space.World);
 
         transform.Rotate(Vector3.forward * _speed * Time.deltaTime);
-        
-       
     }
 
     private void OnTriggerEnter2D(Collider2D other)
